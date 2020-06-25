310b80e61162dd4fc429d395b06ad2561b3207e8
rovedajr <contato@rovedajr.com>
2020-06-24 22:20:40 -0300

    Works as expected. Need to fix the button.

--- scrapbookEngine.js
+++ scrapbookEngine.js
@@ -2,10 +2,15 @@ let field = document.querySelector('#scrapsField')
 let messageTitle = document.querySelector('#messageTitle')
 let messageBody = document.querySelector('#messageBody')
 let button = document.getElementsByTagName("button")[0]
-let tasks = JSON.parse(localStorage.getItem('task_list')) || []
+let tasks =
+    JSON.parse(localStorage.getItem('task_list')) || []
 
-let closer = document.querySelector('.closer')
-console.log(closer);
+function deleteTask(position) {
+    tasks.splice(position, 1)
+
+    renderCard()
+    storeLocally()
+}
 
 
 
@@ -34,6 +39,15 @@ function renderCard() {
         headerBox.classList.add('card-header')
         headerBox.innerText = task.titulo
 
+        let closer = document.createElement('button')
+        closer.innerHTML = `<button type="button" class="close text-white" id="closer" aria-label="Close">
+        <span aria-hidden="true">&times;</span>`
+
+        closer.classList.add('close', 'text-white')
+        headerBox.appendChild(closer)
+
+
+
         let messageBody = document.createElement('div')
         messageBody.classList.add('card-body', 'scroll')
 
@@ -44,6 +58,9 @@ function renderCard() {
         card.appendChild(headerBox)
         card.appendChild(messageBody)
         field.appendChild(card)
+
+        let position = tasks.indexOf(task)
+        closer.setAttribute('onclick', `deleteTask(${position})`)
     }
 }
 

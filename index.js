f07886b092af00cd5b66dbcec26ffbbd85a8531e
rovedajr <contato@rovedajr.com>
2020-06-29 21:04:17 -0300

    Pre modal

--- index.js
+++ index.js
@@ -5,29 +5,44 @@ let scrapsField = document.getElementById("scrapsField");
 
 let scraps = [];
 
+function renderScraps() {
+  scrapsField.innerHTML = ''
+  for (const scrap of scraps) {
+    scrapsField.innerHTML += createScrapCard(scrap.title, scrap.message);
+  }
+}
+
 function addNewScrap() {
   let title = titleInput.value;
   let message = messageInput.value;
 
-  scrapsField.innerHTML = "";
+  if (!messageTitle.value || !messageBody.value) {
+    return alert("Todos os campos deem ser preenchidos!!")
+  }
+
+  titleInput.value = ''
+  messageInput.value = ''
+
 
   scraps.push({ title, message });
 
-  for (const scrap of scraps) {
-    scrapsField.innerHTML = createScrapCard(scrap.title, scrap.message);
-  }
+  renderScraps()
+
 }
 
 function createScrapCard(title, message) {
-  return `
-  <div class="message-cards card text-white bg-dark m-2">
-    <div class="card-header font-weight-bold">${title}</div>
-    <div class="card-body">
-      <p class="card-text">
-        ${message}
-      </p>
-    </div>
+  return `<div class="message-cards card text-white bg-dark m-2">
+  <div class="card-header font-weight-bold">${title}</div>
+  <div class="card-body">
+    <p class="card-text">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. A obcaecati voluptates eligendi enim sequi mollitia, molestias, officia in ad nobis sint cupiditate exercitationem ipsam voluptatibus, debitis quaerat nulla aperiam nihil?
+    </p>
   </div>
+  <div class="w100 d-flex justify-content-end pr-2 pb-2">
+      <button class="btn btn-danger mr-1">deletar</button>
+      <button class="btn btn-info">editar</button>
+  </div>
+</div>
   `;
 }
 

9e85f67e54990bb61e3499267e76a749b26cacfd
rovedajr <contato@rovedajr.com>
2020-06-29 23:08:59 -0300

    Solve the edit button

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 let titleInput = document.getElementById("messageTitle");
+let editTitleInput = document.getElementById('editMessageTitle')
 let messageInput = document.getElementById("messageBody");
+let editMessageInput = document.getElementById('editMessageBody')
 let addButton = document.getElementById("addButton");
 let scrapsField = document.getElementById("scrapsField");
+let btnSaveEdit = document.getElementById("saveEdit")
+
+
 
 let scraps = [];
 
 function renderScraps() {
   scrapsField.innerHTML = ''
   for (const scrap of scraps) {
-    scrapsField.innerHTML += createScrapCard(scrap.title, scrap.message);
+    let position = scraps.indexOf(scrap)
+
+
+    scrapsField.innerHTML += createScrapCard(scrap.title, scrap.message, position);
   }
 }
 
@@ -30,20 +38,41 @@ function addNewScrap() {
 
 }
 
-function createScrapCard(title, message) {
+function createScrapCard(title, message, position) {
   return `<div class="message-cards card text-white bg-dark m-2">
   <div class="card-header font-weight-bold">${title}</div>
   <div class="card-body">
     <p class="card-text">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. A obcaecati voluptates eligendi enim sequi mollitia, molestias, officia in ad nobis sint cupiditate exercitationem ipsam voluptatibus, debitis quaerat nulla aperiam nihil?
+      ${message}
     </p>
   </div>
   <div class="w100 d-flex justify-content-end pr-2 pb-2">
       <button class="btn btn-danger mr-1">deletar</button>
-      <button class="btn btn-info" data-toggle="modal" data-target="#editModal">editar</button>
+      <button class="btn btn-info" onclick="opendEditModal(${position})">editar</button>
   </div>
 </div>
   `;
 }
 
+function opendEditModal(position) {
+  $('#editModal').modal('toggle')
+  editTitleInput.value = scraps[position].title
+  editMessageInput.value = scraps[position].message
+
+  btnSaveEdit.setAttribute('onclick', `saveChanges(${position})`)
+
+}
+
+function saveChanges(position) {
+
+  scraps[position].title = editTitleInput.value
+  scraps[position].message = editMessageInput.value
+  renderScraps()
+
+
+}
+
 addButton.onclick = addNewScrap;
+
+
+// console.log(editTitleInput, editMessageInput);

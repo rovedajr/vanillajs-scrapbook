ca00d79cb4eff86d4a79c6c72443708279723d93
rovedajr <contato@rovedajr.com>
2020-07-08 22:53:19 -0300

    Create class TaskList, constructor method and get dom elements

--- bundle.js
+++ bundle.js
@@ -1,3 +1,16 @@
 "use strict";
 
-alert("teste");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var TaskList = function TaskList() {
+  _classCallCheck(this, TaskList);
+
+  this.titleInput = document.getElementById("messageTitle");
+  this.messageInput = document.getElementById("messageBody");
+  this.addButton = document.getElementById("addButton");
+  this.scrapsField = document.getElementById("scrapsField");
+  this.scraps = [];
+  console.log(this.titleInput, this.messageInput);
+};
+
+new TaskList();
--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert("teste")
+class TaskList {
+    constructor() {
+        this.titleInput = document.getElementById("messageTitle");
+        this.messageInput = document.getElementById("messageBody");
+        this.addButton = document.getElementById("addButton");
+        this.scrapsField = document.getElementById("scrapsField");
+
+        this.scraps = []
+
+        console.log(this.titleInput, this.messageInput);
+
+    }
+}
+
+new TaskList()
